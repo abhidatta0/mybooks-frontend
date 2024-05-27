2c3fa5466bf128f798d0c7d6d8abdf2dff314e94
abhidatta0 <abhidatta146@gmail.com>
2024-05-27 15:38:48 +0530

    updating login page UI

--- src/features/Auth/Login/page.tsx
+++ src/features/Auth/Login/page.tsx
@@ -1,20 +1,26 @@
 const Login = ()=>{
-    return <div className="flex bg-white p-2 w-full border">
-        <div className="w-1/2 bg-blue-300">
-           Left image
+    return <div className="flex bg-white p-2 w-full h-screen border">
+        <div className="w-2/5 relative bg-[url('@/assets/login.jpg')] bg-cover">
+           <div className='absolute bottom-9 left-4'>
+            <p className='text-white text-2xl'>Start your journey with us</p>
+            <p className='text-white text-base w-2/3'>Welcome to Book Haven, your ultimate companion for managing and exploring your personal library. Whether you're an avid reader, a student, or a casual book lover, our app offers a seamless experience to organize your collection, discover new reads, and track your progress.</p>
+           </div>
         </div>
-        <div className="border w-1/2">
-            <p>Welcome Back to Digital</p>
-            <p>Please Login</p>
-            <div className="space-y-3">
-            <div>
-            <label>Email</label>
-            <input type="email" placeholder="name@gmail.com" className="input w-full max-w-xs" />
-            </div>
-            <div>
-            <label>Password</label>
-            <input type="text" placeholder="******" className="input w-full max-w-xs" />
-            </div>
+        <div className="flex justify-center border w-3/5 pt-24">
+            <div className="flex flex-col items-center">
+                <p className="text-2xl">Welcome Back to Book Haven</p>
+                <p>Please Login</p>
+                <div className="space-y-5 w-1/2">
+                <div className="flex flex-col">
+                <label className="text-sm">Email</label>
+                <input type="email" placeholder="name@email.com" className="input input-bordered w-full" />
+                </div>
+                <div className="flex flex-col">
+                <label className="text-sm">Password</label>
+                <input type="text" placeholder="******" className="input input-bordered w-full" />
+                </div>
+                <button className="btn btn-primary w-full">Login</button>
+                </div>
             </div>
         </div>
     </div>
