1342466af184e6a0347d8cc6a9ef1f0ef85694ec
abhidatta0 <abhidatta146@gmail.com>
2024-05-27 13:15:46 +0530

    installed libraries: zod, router, query, hook form , resolver

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 const App = ()=> {
 
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="text-3xl font-bold underline">
       Hello App
     </div>
+    <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
