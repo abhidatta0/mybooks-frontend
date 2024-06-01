dd0bf8c3b2f85eb8b90c428e821c510bfb6facf6
abhidatta0 <abhidatta146@gmail.com>
2024-06-01 20:53:02 +0530

    enhanced home page

--- src/features/Books/page.tsx
+++ src/features/Books/page.tsx
@@ -2,13 +2,17 @@ import { MdOutlineMenuBook } from "react-icons/md";
 import { useState } from 'react';
 import Modal from "@/components/ui/Modal";
 import ListBooks from "./ListBooks/ListBooks";
+import {useAuthUser} from '@/features/Auth/useAuthUser';
 
 const Books = ()=>{
   const [showModal, setShowModal] = useState(false);
+  const {username} = useAuthUser()
 
 
-  return <div>
-    <button className="btn" onClick={()=> setShowModal(true)}>
+  return <div className="p-4">
+    <p className="text-4xl">Happy reading,</p>
+    <p className="text-4xl"> {username}</p>
+    <button className="btn my-3" onClick={()=> setShowModal(true)}>
       <MdOutlineMenuBook size={30}/>
       <p className="text-base">Add new book </p>
     </button>
